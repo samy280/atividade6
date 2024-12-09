44cd7f60fb240e4ad6cd982e5f1aba6a704297ca
samy280 <sms27@aluno.ifal.edu.br>
2024-12-09 11:15:03 -0300

    Q1html.js
    ta o html e o js juntos

---
+++ Q1html.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+    <script src="Js/scripts.js"></script>
+</body>
+</html>
+
+‎scripts.js
+
+for(var i = 1; i < 20; i++){
+        console.log("Eu gosto de estudar Algoritmos!");
+  {
+          
